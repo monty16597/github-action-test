b32eadcbf0bec2bb93735e333fc943e048439790
Manjeetsinh Alonja <alonjamanjeetsinh@lambdatest.com>
2022-08-02 14:51:34 +0530

    Added ec2

--- main.tf
+++ main.tf
@@ -10,3 +10,13 @@ resource "aws_s3_bucket" "b_2" {
   bucket = "prod-test-123-v1"
             acl = "private"
 }
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t1.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
