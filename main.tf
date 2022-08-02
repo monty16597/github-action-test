4fb89322ca848f8a697b355d44c23d8e06dcc19e
Manjeetsinh Alonja <alonjamanjeetsinh@lambdatest.com>
2022-08-02 14:49:38 +0530

    minor fix

--- main.tf
+++ main.tf
@@ -4,3 +4,9 @@ resource "aws_s3_bucket" "b-1" {
   bucket = "s3-website-test-hashicorp1com"
             acl = "private"
 }
+
+
+resource "aws_s3_bucket" "b_2" {
+  bucket = "prod-test-123-v1"
+            acl = "private"
+}
